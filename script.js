f7fc668c79234e0cf2b6b3f157935763ef015b67
Marvelousmolokwu <qmolokwu@gmail.com>
2023-01-26 22:09:25 +0100

    first assignment on data structure

---
+++ script.js
@@ -0,0 +1,34 @@
+const array = [];
+// length of array
+let length = 6;
+let sizeOfIndex = length - 1;
+let last = 2;
+// lf length -1 is the last index then a space is empty i
+// the last position in array that is filled
+
+function isEmpty() {
+  if (length - (last + 1) > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isEmpty());
+function isFull() {
+  if (length - last === 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isFull());
+function isAdd() {
+  let empty = isEmpty();
+  let full = isFull();
+  if (empty === true && full === false) {
+    return last+=1;
+  } else {
+    return false;
+  }
+}
+console.log(isAdd());
